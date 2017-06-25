9fc68cc2506c660b9451094b1fea0eb97d80313d
lxdnz254 <lxdnz34@gmail.com>
2017-06-25 14:51:22 +1200

    Read Person file and report leap year born people

--- AddingAMethod/Program.cs
+++ AddingAMethod/Program.cs
@@ -12,7 +12,8 @@ namespace AddingAMethod
 		{
 			Console.WriteLine("Welcome to the first program with classes and ojects");
 			Person p = new Person("John", "Key", "9-August-1996", 111); //Creating the first object
-			Person q = p;
+			Person q = new Person("Brain", "Adam", 2222);
+			Person r = new Person ();
 			p.displayInfo(); //Calling the display method in person
 			p.AccessFirstName = "Johnson";
 			Console.WriteLine ("To get the value of the first name us: " + p.AccessFirstName);
---
+++ ConstructPersonsFromFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConstructPersonsFromFile
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hi, this is the program to parse a list of people");
+			Person[] personArray = ReadFromFile.personList (); // get the list of people
+
+			// iterate over array. If person is born in leap year output their first name
+			for (int i = 0; i < personArray.Length; i++) {
+				if (personArray [i].isLeapYear ()) {
+					Console.WriteLine (personArray [i].AccessFirstName + " is born in a leap year.");
+				}
+			} // end of for loop
+
+			// MonoDev does this automatically so it will repeat
+			Console.WriteLine("Press any key to continue ...");
+			Console.ReadKey ();
+		} // end of main
+	} // end of class
+} // end of namespace
