adbf78172de0391c88b847549702d474c558bd02
lxdnz254 <lxdnz34@gmail.com>
2017-06-25 14:05:36 +1200

    StaticExample

---
+++ StaticClassExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassExample
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hi and welcome to this example which will show a demonstration of a static class");
+			Console.WriteLine ("Enter a number");
+			String line = Console.ReadLine ();
+			int n = int.Parse (line);
+			if (Utilities.isPrime (n)) {
+				Console.WriteLine ("Hi, the number you entered was " + n + " and it is a prime number");
+			} else {
+				Console.WriteLine ("Hi, the number you entered was " + n + " and is not a prime number");
+			}
+			Console.WriteLine (Utilities.pi);
+
+		} // end of main
+	} // end of the class
+} // end of namespace
---
+++ StaticClassExample/Utilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassExample
+{
+	public static class Utilities
+	{
+		public static double pi = 3.14159265;
+		public static Boolean isPrime(int n)
+		{
+			Boolean res = true;
+			for (int i = 2; i < Math.Sqrt (n); i++) 
+			{
+				if ((n % i) == 0) 
+				{
+					res = false;
+					break;
+				}
+			} // end of for loop
+			return (res);
+		} // end of isPrime method
+	} // end of Utilities class
+} // end of namespace
+
