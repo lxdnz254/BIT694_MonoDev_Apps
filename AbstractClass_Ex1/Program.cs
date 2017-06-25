7b0a6a8d8636cc15d80f21145524c4ce7192ab5c
lxdnz254 <lxdnz34@gmail.com>
2017-06-25 18:34:48 +1200

    Interface and Abstract examples

---
+++ AbstractClass_Ex1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass_Ex1
+{
+	abstract class AbstractClass
+	{
+		public int n = 17;
+		public int add(int a, int b) {
+			return (a + b + n);
+		} // default implementation
+	}
+
+	class RegularClass: AbstractClass
+	{
+		public RegularClass() {
+		}
+	//	public int add(int a, int b) {
+	//		return (a + b + 6);
+	//	} // the "real" implementation
+	}
+
+	class MainClass
+	{
+		static void Main (string[] args)
+		{
+			Console.WriteLine ("Welcome to a simple example of abstract classes");
+				RegularClass ex = new RegularClass ();
+				Console.WriteLine ("The sum of 1 and 2 is: " + ex.add (1, 2));
+				Console.WriteLine ("Press any key to continue ....");
+				Console.ReadKey ();
+		}
+	}
+}
---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+	class MainClass
+	{
+		interface InterfaceExample
+		{
+			int add(int a, int b); //This interface does not include real code
+		}
+
+		class ClassExample: InterfaceExample
+		{
+			public int add(int a, int b) {return (a+b);} // An implemenation of the interface
+		}
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Welcome to an Interface Example in C#");
+			ClassExample ex = new ClassExample ();
+			Console.WriteLine ("The sum of 5 and 6 is: " + ex.add (5, 6));
+			Console.WriteLine ("Press any key to continue ....");
+			Console.ReadKey ();
+		}
+	}
+}
