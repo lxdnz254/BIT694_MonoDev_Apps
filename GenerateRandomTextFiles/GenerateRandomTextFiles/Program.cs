a934f22717b38514c78cea8c6184ccf41fbe0dbf
lxdnz254 <lxdnz34@gmail.com>
2017-09-05 21:57:44 +1200

    Added parallel Async writing Tasks for files, this way the files can be written in background without affecting UI or other threads.

--- GenerateRandomTextFiles/GenerateRandomTextFiles/Program.cs
+++ GenerateRandomTextFiles/GenerateRandomTextFiles/Program.cs
@@ -13,17 +13,11 @@ namespace GenerateRandomTextFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Random Text File Generator - version 1, press any key to start");
+            Console.WriteLine("Random Text File Generator - version 2 (using parallel async writing)\r\nPress any key to start");
             Console.ReadKey();
 
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-            // the code that you want to measure comes here
-           
-            Random random = new Random();
-            string script;
-            int folderNo;
-            int wordCount;
-            int databaseWordID;
+            var dbWatch = System.Diagnostics.Stopwatch.StartNew();          
+            
             List<String> wordList = new List<String>();
 
             // Import the NewWords database into the project firs "Project -> Add New Data Source"
@@ -91,50 +85,89 @@ namespace GenerateRandomTextFiles
                     wordList.Add(syn);
                 }
             }
+            dbWatch.Stop();
 
-            // Generate 1000 text files
-            for (int k=1; k <=1000; k++)
-            {
-                // random folder number
-                folderNo = random.Next(7);
-                // random word count min/max
-                wordCount = random.Next(9,31);
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            // the code that you want to measure comes here
 
-                string[] text = new string[wordCount];
-                script = "";
-
-                for (int j = 0; j < text.Length; j++)
-                {
-                    // random word from database
-                    databaseWordID = random.Next(wordList.Count);
-                    text[j] = wordList[databaseWordID];
-                    script += text[j] + " ";
-                }
-
-                // Create folders and file string
-                string folderOutput = "C:\\TestFolder\\folder" + folderNo;
-                Directory.CreateDirectory(folderOutput);
-                string fileOutput = folderOutput + "\\test_file" + k + ".txt";
-
-                //How to write to a file. When writing to a file, if the file does not exist, the program creates it.
-                FileStream fs = new FileStream(fileOutput, FileMode.Create, FileAccess.Write, FileShare.ReadWrite); // FileMode.Create overwites an existing file
-                TextWriter tw = new StreamWriter(fs);
-                StreamWriter sw = new StreamWriter(fs);
-                tw.WriteLine(script);
-                tw.Flush();
-               
-                // Outputs the files so you know its working, this can be commented out
-                Console.WriteLine("Folder:" + folderNo + ", Word Count: " + wordCount);
-                Console.WriteLine("Text:" + script);
-
-            }
+            ProcessWriteMulti(wordList).Wait();
+            
             // end the timer
             watch.Stop();
+            var dbElapsed = dbWatch.ElapsedMilliseconds;
             var elapsedMs = watch.ElapsedMilliseconds;
-            Console.WriteLine("Speed of processing: " + elapsedMs.ToString());
+            Console.WriteLine("\r\nSpeed of initialisation: " + dbElapsed.ToString() + "ms");
+            Console.WriteLine("Speed of file writing: " + elapsedMs.ToString()+"ms");
+            Console.WriteLine("Total Time: " + (elapsedMs + dbElapsed).ToString() + "ms");
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         
+        }
+
+        static async Task ProcessWriteMulti(List<String> wordList)
+        {
+            Random random = new Random();
+            string script;
+            int folderNo;
+            int wordCount;
+            int databaseWordID;
+            List<Task> tasks = new List<Task>();
+            List<FileStream> sourceStreams = new List<FileStream>();
+
+            try
+            {
+                // Generate 1000 text files
+                for (int k = 1; k <= 1000; k++)
+                {
+                    // random folder number
+                    folderNo = random.Next(7);
+                    // random word count min/max
+                    wordCount = random.Next(9, 31);
+
+                    string[] text = new string[wordCount];
+                    script = "";
+
+                    for (int j = 0; j < text.Length; j++)
+                    {
+                        // random word from database
+                        databaseWordID = random.Next(wordList.Count);
+                        text[j] = wordList[databaseWordID];
+                        script += text[j] + " ";
+                    }
+
+                    // Create folders and file string
+                    string folderOutput = "C:\\TestFolder\\folder" + folderNo;
+                    Directory.CreateDirectory(folderOutput);
+                    string fileOutput = folderOutput + "\\test_file" + k + ".txt";
+
+                    byte[] encodeText = Encoding.Unicode.GetBytes(script);
+
+                    //How to write to a file. When writing to a file, if the file does not exist, the program creates it.
+                    FileStream fs = new FileStream(fileOutput, FileMode.Create, FileAccess.Write, FileShare.ReadWrite, bufferSize: 4096, useAsync: true); // FileMode.Create overwites an existing file
+
+                    Task theTask = fs.WriteAsync(encodeText, 0, encodeText.Length);
+                    sourceStreams.Add(fs);
+
+                    tasks.Add(theTask);
+
+                    /* Outputs the files so you know its working, this can be commented out
+                    Console.WriteLine("Folder:" + folderNo + ", Word Count: " + wordCount);
+                    Console.WriteLine("Text:" + script);
+                    */
+
+                }
+                await Task.WhenAll(tasks);
+            }
+            finally
+            {
+                foreach (FileStream sourceStream in sourceStreams)
+                {
+                    sourceStream.Flush();
+                }
+            }
+
+            
+            
         }
     }
 }
